994855f76a3d3b094a70fa18609c0e949f3050ac
OliverP <opaucarq@uni.pe>
2023-12-04 10:56:11 -0500

    update responsive search and add client

--- frontend/src/components/manageClient/index.tsx
+++ frontend/src/components/manageClient/index.tsx
@@ -1,24 +1,27 @@
 import { TbSearch } from 'react-icons/tb'
 function ManageClient() {
   return (
-    <div>
-      <h3>Gestion de clientes</h3>
+    <div className='flex flex-col gap-4 bg-black bg-opacity-80 px-4 py-4 md:px-12'>
+      <h3 className='font-bold text-3xl text-[#F5F1EA]'>Gestion de clientes</h3>
       <div className='flex items-center justify-between'>
-        <div className='flex-col gap-2 md:flex md:flex-row'>
-          <div className='flex items-center border border-gray-300 rounded p-2'>
+        <div className='flex flex-col gap-2 w-52 md:flex-row md:w-auto'>
+          <div className='flex items-center border bg-white border-gray-300 rounded p-1 w-full md:w-52'>
             <input
-              className='outline-none px-2 py-1 flex-grow'
+              className='outline-none py-1'
               type="text"
               placeholder='Buscar cliente'
             />
-            <div className='ml-2'>
+            <div className=''>
               <TbSearch className='text-gray-800 text-xl' />
             </div>
           </div>
-          <button className='bg-[#354762] text-[#FFFDFD] px-2 py-1 rounded-md'>Buscar</button>
+          <button 
+            className='bg-[#354762] text-[#FFFDFD] w-full py-2 rounded-lg md:w-52'>
+              Buscar
+          </button>
         </div>
         <div>
-          <button className='bg-[#354762] text-[#FFFDFD] px-2 py-1 rounded-md'>Agregar Cliente</button>
+          <button className='bg-[#354762] text-[#FFFDFD] w-52 py-2 rounded-lg'>Agregar Cliente</button>
         </div>
       </div>
     </div>
