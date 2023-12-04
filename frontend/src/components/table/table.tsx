2682aaf0175768765bd857b885447678dba61a21
OliverP <opaucarq@uni.pe>
2023-12-03 21:11:48 -0500

    update table structure

--- frontend/src/components/table/table.tsx
+++ frontend/src/components/table/table.tsx
@@ -1,7 +1,47 @@
 
 function Table() {
   return (
-    <div>Table</div>
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Email</th>
+          <th>Teléfono</th>
+          <th>Dirección</th>
+          <th>Editar</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Nombre Apellido</td>
+          <td>Email@email.com</td>
+          <td>111-222-333</td>
+          <td>Dirección de su oficina 000</td>
+          <td>...</td>
+        </tr>
+        <tr>
+          <td>Nombre Apellido</td>
+          <td>Email@email.com</td>
+          <td>111-222-333</td>
+          <td>Dirección de su oficina 000</td>
+          <td>...</td>
+        </tr>
+        <tr>
+          <td>Nombre Apellido</td>
+          <td>Email@email.com</td>
+          <td>111-222-333</td>
+          <td>Dirección de su oficina 000</td>
+          <td>...</td>
+        </tr>
+        <tr>
+          <td>Nombre Apellido</td>
+          <td>Email@email.com</td>
+          <td>111-222-333</td>
+          <td>Dirección de su oficina 000</td>
+          <td>...</td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
