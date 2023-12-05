101fe8abaf5311c6473e191d6f7bba327e8b68b1
Jhony A Saenz <jash0310@gmail.com>
2023-12-04 20:37:54 -0500

    agregando CRUD proveedor

---
+++ backend/src/controller/supplier.controllers.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from 'express';
+import { deleteByIdSupplier, findAllSuppliers, getByIdSupplier, insertSupplier, updateByIdSupplier } from '../services/suppliers.services';
+
+const getSuppliers = async (req: Request, res: Response) => {
+  try {
+    const suppliers = await findAllSuppliers();
+    res.status(200).json(suppliers);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const postSupplier = async (req: Request, res: Response) => {
+  try {
+    const bodySupplier = req.body;
+    const supplier = await insertSupplier(bodySupplier);
+    res.status(201).json(supplier);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getSuuplierById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const supplier = await getByIdSupplier(parseInt(id));
+    res.status(200).json(supplier);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateSupplier = async (req: Request, res: Response) => {
+  try {
+    const { proveedor_id } = req.params;
+    const { nombre, direccion, telefono } = req.body;
+    const supplier = await updateByIdSupplier(parseInt(proveedor_id), { nombre, direccion, telefono });
+    res.status(200).json(supplier);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteSupplier = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const supplier = await deleteByIdSupplier(parseInt(id));
+    res.status(200).json(supplier);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getSuppliers, postSupplier, getSuuplierById, updateSupplier, deleteSupplier };
