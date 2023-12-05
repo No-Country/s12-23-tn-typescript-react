ca440e46aa533a8a442ffbb8ebf584e445960e22
De1t4 <mauriciochambicaceres@gmail.com>
2023-12-05 11:12:12 -0300

    added link home

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div>login</div>
+    <>
+      <h1>login</h1>
+      <Link to={"/home"}>Incio</Link>
+    </>
   )
 }
